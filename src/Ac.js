5c8cd66ec907e080df138c8fca146a647ef78a14
Shathees1234 <shatheesg833@gmail.com>
2025-09-16 14:54:26 +0530

    Update Ac.js

--- src/Ac.js
+++ src/Ac.js
@@ -1,15 +1,1 @@
-import React, { Suspense } from 'react';
-const LazyLoading=React.lazy(()=>import('./LazyLoading.js')
-    
-)
-function Ac(){
-    return(
-        <>
-            <Suspense fallback={<h1>Loading......</h1>}>
-                 <LazyLoading/>
-            </Suspense>
-           
-        </>
-    )
-}
-export default Ac;
+
